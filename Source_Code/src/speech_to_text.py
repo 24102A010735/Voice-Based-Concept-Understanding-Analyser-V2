bea4423928bf3da66a9fb3febcab6eebde571aa9
takarthik149 <takarthik149@gmail.com>
2026-06-29 07:42:16 +0530

    Fix speech_to_text

--- Source_Code/src/speech_to_text.py
+++ Source_Code/src/speech_to_text.py
@@ -1,31 +1,4 @@
-# import whisper
-# import tempfile
-# import os
-
-# class SpeechToText:
-
-#     def __init__(self):
-#         print("Loading Whisper...")
-#         self.model = whisper.load_model("tiny")
-#         print("Whisper Loaded")
-
-#     def transcribe_audio(self, uploaded_file):
-
-#         suffix = os.path.splitext(uploaded_file.name)[1]
-
-#         with tempfile.NamedTemporaryFile(delete=False, suffix=suffix) as tmp:
-#             tmp.write(uploaded_file.read())
-#             audio_path = tmp.name
-
-#         print("Audio Saved")
-
-#         print("Starting Whisper...")
-
-#         result = self.model.transcribe(audio_path)
-
-#         print("Whisper Finished")
-
-#         return result["text"], audio_pathimport whisper
+import whisper
 import tempfile
 import os
 import time
@@ -33,30 +6,28 @@ import time
 class SpeechToText:
 
     def __init__(self):
-
         print("STEP 1")
+        start = time.time()
 
-        start=time.time()
-
-        self.model=whisper.load_model("tiny")
+        self.model = whisper.load_model("tiny")
 
         print("STEP 2")
-        print(time.time()-start)
+        print("Loaded in", time.time() - start, "seconds")
 
-    def transcribe_audio(self,uploaded_file):
+    def transcribe_audio(self, uploaded_file):
 
         print("STEP 3")
 
-        suffix=os.path.splitext(uploaded_file.name)[1]
+        suffix = os.path.splitext(uploaded_file.name)[1]
 
-        with tempfile.NamedTemporaryFile(delete=False,suffix=suffix) as tmp:
+        with tempfile.NamedTemporaryFile(delete=False, suffix=suffix) as tmp:
             tmp.write(uploaded_file.read())
-            audio_path=tmp.name
+            audio_path = tmp.name
 
         print("STEP 4")
 
-        result=self.model.transcribe(audio_path)
+        result = self.model.transcribe(audio_path)
 
         print("STEP 5")
 
-        return result["text"],audio_path
+        return result["text"], audio_path
